ac891a1e113a88b2fb9b9b88a7a1ff9960e594d2
PranavPonni <pranavponni2003@gmail.com>
2026-03-17 15:31:04 +0900

    teleop virtual line break

--- allegro_leader/example/teleop_virtual.py
+++ allegro_leader/example/teleop_virtual.py
@@ -3,7 +3,6 @@ import numpy as np
 import copy
 from allegro_leader import AllegroPrecesionGrasp, AllegroVis
 
-
 class TeleopRunner:
     def __init__(self, side="left", wall_kp=0., rate_hz=50):
         self.teleop = AllegroPrecesionGrasp(wall_kp=wall_kp)
@@ -11,7 +10,6 @@ class TeleopRunner:
         self.q = copy.deepcopy(self.vis.q0)
         self.dt = 1.0 / rate_hz
         self.finger_idx = [0, 1, 2, 3, 4, 5, 6, 7]
-
 
     def run(self):
         try:
